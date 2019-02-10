fa97ca6cdfdf7460e0bc1fb98759bdff40bf6934
Megacrafter127 <player4596@web.de>
2019-02-10 23:27:10 +0100

    query optimization the 3rd

--- edd-gasfinder.sql
+++ edd-gasfinder.sql
@@ -1,15 +1,16 @@
 SELECT allFound.EventTime, allFound.SystemName
 FROM (
-		SELECT EventTime, json_extract(EventData, '$.SystemName') AS SystemName
+		SELECT MAX(EventTime) AS EventTime, json_extract(EventData, '$.SystemName') AS SystemName
 		FROM JournalEntries
 		WHERE EventType IS 'FSSAllBodiesFound'
+		GROUP BY SystemName
 	) AS allFound -- nested query to eliminate as many lines as possible before the join, for speed
 	INNER JOIN (
-		SELECT EventData
+		SELECT json_extract(EventData, '$.BodyName') AS BodyName
 		FROM JournalEntries
 		WHERE EventType IS 'Scan'
+		AND json_extract(EventData, '$.StarType') IN ('K', 'F', 'G')
 	) AS starCandidates -- nested query to eliminate as many lines as possible before the join, for speed
-	ON json_extract(starCandidates.EventData, '$.BodyName') IS allFound.SystemName
-WHERE json_extract(starCandidates.EventData, '$.StarType') IN ('K', 'F', 'G')
-GROUP BY allFound.SystemName -- Make eliminating stray duplicate entries easier.
+	ON starCandidates.BodyName IS allFound.SystemName
+	
 ORDER BY allFound.EventTime DESC
