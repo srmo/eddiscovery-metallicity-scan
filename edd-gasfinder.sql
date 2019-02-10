f3d5fb00c810f95e6d933672d0bffc60beefdc47
Megacrafter127 <player4596@web.de>
2019-02-10 13:57:57 +0100

    fixed order for json-extraction comparisons; some query optimization

--- edd-gasfinder.sql
+++ edd-gasfinder.sql
@@ -1,14 +1,15 @@
-SELECT allFound.EventTime,
- json_extract(allFound.EventData, '$.SystemName') as SystemName
-	FROM JournalEntries AS allFound, 
-		 json_each(allFound.EventData) AS jsonAllFound
-	INNER JOIN JournalEntries AS starCandidates ON 
-	-- Only check the primary star. TGMS doesn't care about secondary stars of the right type.
-	-- Also list systems without gas giants. Whether the gas giant exists is a datapoint wanted by the survey.
-		starCandidates.EventType='Scan'
-		AND   json_extract(starCandidates.EventData, '$.BodyName') IS jsonAllFound.value
-		AND   json_extract(starCandidates.EventData, '$.StarType') IN ('K', 'F', 'G')
-WHERE allFound.EventType='FSSAllBodiesFound'
-AND	  jsonAllFound.key='SystemName'
-GROUP BY SystemName -- Make eliminating stray duplicate entries easier.
-ORDER BY allfound.EventTime DESC
+SELECT allFound.EventTime, allFound.SystemName
+FROM (
+		SELECT EventTime, json_extract(EventData, '$.SystemName') AS SystemName
+		FROM JournalEntries
+		WHERE EventType IS 'FSSAllBodiesFound'
+	) AS allFound -- nested query to eliminate as many lines as possible before the join, for speed
+	INNER JOIN (
+		SELECT EventData
+		FROM JournalEntries
+		WHERE EventType IS 'Scan'
+	) AS starCandidates -- nested query to eliminate as many lines as possible before the join, for speed
+	ON json_extract(starCandidates.EventData, '$.BodyName') IS allFound.SystemName
+	AND json_extract(starCandidates.EventData, '$.StarType') IN ('K', 'F', 'G')
+GROUP BY allFound.SystemName -- Make eliminating stray duplicate entries easier.
+ORDER BY allFound.EventTime DESC
