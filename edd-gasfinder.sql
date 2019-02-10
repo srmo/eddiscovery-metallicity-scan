47ec2c5e4d62660efb3a092df43d299076a5e645
unknown <srm@schakalacka.org>
2019-02-10 23:42:39 +0100

    Replace outer GROUP BY with DISTINCT on second sub
    - better readibility
    - benchmarking vs outer GROUP BY pending :)

--- edd-gasfinder.sql
+++ edd-gasfinder.sql
@@ -1,4 +1,4 @@
-SELECT max(allFound.EventTime), allFound.SystemName
+SELECT allFound.EventTime, allFound.SystemName
 FROM (
 		SELECT MAX(EventTime) AS EventTime, json_extract(EventData, '$.SystemName') AS SystemName
 		FROM JournalEntries
@@ -6,11 +6,10 @@ FROM (
 		GROUP BY SystemName
 	) AS allFound -- nested query to eliminate as many lines as possible before the join, for speed
 	INNER JOIN (
-		SELECT json_extract(EventData, '$.BodyName') AS BodyName
+		SELECT DISTINCT json_extract(EventData, '$.BodyName') AS BodyName
 		FROM JournalEntries
 		WHERE EventType IS 'Scan'
 		AND json_extract(EventData, '$.StarType') IN ('K', 'F', 'G')
 	) AS starCandidates -- nested query to eliminate as many lines as possible before the join, for speed
 	ON starCandidates.BodyName IS allFound.SystemName
-GROUP BY SystemName	
 ORDER BY allFound.EventTime DESC
